aa945dc893e1904411c07d48ed8662e2a9291fa4
Lucas Beitel <lucasmayimbeitel@gmail.com>
2025-02-08 11:26:56 -0800

    excel file reading part 1

--- frontend/app/src/main/java/com/example/get2class/MainActivity.kt
+++ frontend/app/src/main/java/com/example/get2class/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.get2class
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Upload Schedule Button
+        findViewById<Button>(R.id.upload_schedule).setOnClickListener {
+            val intent = Intent(this, ScheduleUpload::class.java)
+            startActivity(intent)
         }
     }
 }
