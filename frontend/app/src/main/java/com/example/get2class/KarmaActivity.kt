c3a4a096854be5b30391e467d30dbd20db27bb54
trevdang <kfchut12345@gmail.com>
2025-02-18 23:30:52 -0800

    Initialize front end skeleton code

---
+++ frontend/app/src/main/java/com/example/get2class/KarmaActivity.kt
@@ -0,0 +1,20 @@
+package com.example.get2class
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class KarmaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_karma)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.karma)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ frontend/app/src/main/java/com/example/get2class/ScheduleListActivity.kt
@@ -0,0 +1,20 @@
+package com.example.get2class
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ScheduleListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_schedule_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.schedule_list)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
