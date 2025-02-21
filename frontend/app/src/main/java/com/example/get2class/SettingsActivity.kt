a68daefd216aa5a4115bd7a8686f1eb57b4388ca
trevdang <kfchut12345@gmail.com>
2025-02-21 01:24:13 -0800

    Add persistent user settings and logic for saving user settings

--- frontend/app/src/main/java/com/example/get2class/SettingsActivity.kt
+++ frontend/app/src/main/java/com/example/get2class/SettingsActivity.kt
@@ -1,12 +1,33 @@
 package com.example.get2class
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "SettingsActivity"
+    }
+
+    private lateinit var notification_switch: Switch
+    private lateinit var edit_minutes: EditText
+    private lateinit var save_button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +37,94 @@ class SettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        notification_switch = findViewById(R.id.notifications_switch)
+        edit_minutes = findViewById(R.id.edit_minutes)
+        save_button = findViewById(R.id.save_settings_button)
+
+        getNotificationSettings(BuildConfig.BASE_API_URL + "/get_notification_settings" + "?sub=" + LoginActivity.GoogleIdTokenSub) { result ->
+            Log.d(TAG, "getNotificationSettings: $result")
+
+            val notificationsEnabled = result.getBoolean("notificationsEnabled")
+            val notificationTime = result.getString("notificationTime")
+
+            runOnUiThread {
+                notification_switch.setChecked(notificationsEnabled)
+                edit_minutes.setText(notificationTime)
+            }
+        }
+
+        save_button.setOnClickListener() {
+            saveNotificationSettings(BuildConfig.BASE_API_URL + "/save_notification_settings") { result ->
+                Log.d(TAG, "Saving notification settings...")
+                Log.d(TAG, "$result")
+
+                val acknowledgement = result.getBoolean("acknowledged")
+                runOnUiThread {
+                    if (acknowledgement) {
+                        Toast.makeText(this, result.getString("message"), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "An error has occurred", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    fun getNotificationSettings(url: String, callback: (JSONObject) -> Unit) {
+        // Create GET request for OkHttp3
+        val request = Request.Builder().url(url).get().build()
+
+        // Make call
+        ApiService.client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d(TAG, "Error: $e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val result = response.body()?.string()
+                if (result != null) {
+                    try {
+                        val jsonObject = JSONObject(result)
+                        callback(jsonObject)
+                    } catch (_: Exception) {
+                        val badJsonObject = JSONObject()
+                        callback(badJsonObject)
+                    }
+                }
+            }
+        })
+    }
+
+    fun saveNotificationSettings(url: String, callback: (JSONObject) -> Unit) {
+        // Create JSONObject to send
+        val jsonObject = JSONObject()
+        jsonObject.put("sub", LoginActivity.GoogleIdTokenSub)
+        jsonObject.put("notificationsEnabled", notification_switch.isChecked)
+        jsonObject.put("notificationTime", edit_minutes.text)
+
+        // Create RequestBody and Request for OkHttp3
+        val body = RequestBody.create(ApiService.JSON, jsonObject.toString())
+        val request = Request.Builder().url(url).put(body).build()
+
+        // Make call
+        ApiService.client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d(TAG, "Error: $e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val result = response.body()?.string()
+                if (result != null) {
+                    try {
+                        val jsonObject = JSONObject(result)
+                        callback(jsonObject)
+                    } catch (_: Exception) {
+                        val badJsonObject = JSONObject()
+                        callback(badJsonObject)
+                    }
+                }
+            }
+        })
     }
 }
