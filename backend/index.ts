2ba3d160178a30baeb6e1d23d18d58b2b86c6aec
trevdang <kfchut12345@gmail.com>
2025-02-22 02:26:24 -0800

    Add store schedule and get schedule routes and refactor create new user route initialization

--- backend/index.ts
+++ backend/index.ts
@@ -32,16 +32,27 @@ app.get('/find_existing_user', async (req: Request, res: Response) => {
 
 app.post('/create_new_user', async (req: Request, res: Response) => {
     try {
-        const requestBody = {
-            "email": req.body["email"],
-            "sub": req.body["sub"],
-            "name": req.body["name"],
-            "karma": 0,
-            "notificationTime": 15,
-            "notificationsEnabled": true
+        const userRequestBody = {
+            email: req.body["email"],
+            sub: req.body["sub"],
+            name: req.body["name"],
+            karma: 0,
+            notificationTime: 15,
+            notificationsEnabled: true
         };
 
-        const data = await client.db("get2class").collection("users").insertOne(requestBody);
+        const courseListRequestBody = {
+            email: req.body["email"],
+            sub: req.body["sub"],
+            name: req.body["name"],
+            fallCourseList: [],
+            winterCourseList: [],
+            summerCourseList: []
+        };
+
+        const userData = await client.db("get2class").collection("users").insertOne(userRequestBody);
+        const scheduleData = await client.db("get2class").collection("schedules").insertOne(courseListRequestBody);
+
         res.status(200).json({ "data": "Successfully registered account" });
     } catch (err) {
         console.error(err);
@@ -64,7 +75,74 @@ app.post('/tokensignin', async (req: Request, res: Response) => {
         console.error(err);
         res.status(500).json({ "err": err });
     }
-})
+});
+
+/**
+ * Schedules related routes
+ */
+app.get('/get_schedule', async (req: Request, res: Response) => {
+    try {
+        const sub = req.query["sub"];
+        const term = req.query["term"];
+
+        let courseList = "";
+        if (term == "fallCourseList") courseList = "fallCourseList";
+        else if (term == "winterCourseList") courseList = "winterCourseList";
+        else courseList = "summerCourseList";
+
+        const data = await client.db("get2class").collection("schedules").findOne({ sub: sub });
+
+        if (data != null) {
+            res.status(200).json({ "courseList": data[courseList] });
+        } else {
+            throw Error("data is null");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.put('/store_schedule', async (req: Request, res: Response) => {
+    try {        
+        const sub = req.body["sub"];
+        let document;
+        
+        const filter = {
+            sub: sub
+        };
+
+        if (req.body["fallCourseList"]) {
+            document = {
+                $set: {
+                    fallCourseList: req.body["fallCourseList"]
+                }
+            };
+        } else if (req.body["winterCourseList"]) {
+            document = {
+                $set: {
+                    winterCourseList: req.body["winterCourseList"]
+                }
+            };
+        } else {
+            document = {
+                $set: {
+                    summerCourseList: req.body["summerCourseList"]
+                }
+            };
+        };
+
+        const options = {
+            upsert: false
+        };
+
+        const data = await client.db("get2class").collection("schedules").updateOne(filter, document, options);
+        res.status(200).json({ acknowledged: data.acknowledged, message: "Successfully uploaded schedule" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err)
+    }
+});
 
 /**
  * Mongo and Express connection setup
