6dfb231e6617748e00b855668fbe9e840dbe88c0
Luke <lukematson39@gmail.com>
2025-03-10 13:04:04 -0700

    bug fixes on first schedule test

--- backend/tests/unmocked_tests/schedule.test.ts
+++ backend/tests/unmocked_tests/schedule.test.ts
@@ -1,5 +1,6 @@
-import request from "supertest";
-import { app } from "../../index";
+const request = require("supertest");
+const { app } = require("../../index");
+
 
 describe("Unmocked: PUT /schedule", () => {
     test("Empty request body", async () => {
