eaef5234e0f7287d92b9c3a04bfa1886b6062de1
trevdang <kfchut12345@gmail.com>
2025-03-11 22:38:58 -0700

    Add GetUser tests

--- backend/tests/unmocked_tests/GetUser.test.ts
+++ backend/tests/unmocked_tests/GetUser.test.ts
@@ -1,8 +1,12 @@
-import { app } from '../../index';
+import { serverReady, cronResetAttendance } from '../../index';
 import { client } from '../../services';
 import request from 'supertest';
 
+let server: any;
+
 beforeAll(async () => {
+    server = await serverReady;
+
     await client.db("get2class").collection("users").insertOne({ 
         email: "asdfasdf@gmail.com",
         sub: "123",
@@ -18,23 +22,35 @@ afterAll(async () => {
         sub: "123"
     });
     await client.close();
+    cronResetAttendance.stop();
+    await server.close();
 });
 
 describe("Unmocked: GET /user", () => {
     test("Found valid user", async () => {
-        const res = await request(app).get("/user").query({sub: "123"});
+        const res = await request(server).get("/user").query({sub: "123"});
 
         expect(res.statusCode).toBe(200);
         expect(res.body).toHaveProperty('email');
+        expect(res.body).toHaveProperty('sub');
+        expect(res.body).toHaveProperty('name');
+        expect(res.body).toHaveProperty('karma');
+        expect(res.body).toHaveProperty('notificationTime');
+        expect(res.body).toHaveProperty('notificationsEnabled');
     });
 
     test("No valid user", async () => {
-        const res = await request(app).get("/user").query({sub: "blahblah"});
+        const res = await request(server).get("/user").query({sub: "blahblah"});
+
         expect(res.statusCode).toBe(400);
+        expect(res.text).toBe("User does not exist");
+        expect(res.body).toEqual({});
     });
 
     test("Empty query", async () => {
-        const res = await request(app).get("/user").query({});
+        const res = await request(server).get("/user").query({});
+
         expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty('errors');
     });
 });
