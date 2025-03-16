e16ed4ff528f3b2022bb0a1cfbe8a4ca703933be
trevdang <kfchut12345@gmail.com>
2025-03-16 00:16:29 -0700

    Add mocked database error tests for GetUser

---
+++ backend/tests/mocked_tests/GetUser.test.ts
@@ -0,0 +1,62 @@
+import { serverReady, cronResetAttendance } from '../../index';
+import { client } from '../../services';
+import request from 'supertest';
+
+let server: any;
+
+beforeAll(async () => {
+    server = await serverReady;
+
+    await client.db("get2class").collection("users").insertOne({ 
+        email: "asdfasdf@gmail.com",
+        sub: "123",
+        name: "asdfasdf",
+        karma: 0,
+        notificationTime: 15,
+        notificationsEnabled: true
+     });
+});
+
+afterAll(async () => {
+    await client.db("get2class").collection("users").deleteOne({
+        sub: "123"
+    });
+
+    await client.close();
+    await cronResetAttendance.stop();
+    await server.close();
+});
+
+describe("Mocked: GET /user", () => {
+    test("Unable to reach get2class database when getting user", async () => {
+        const dbSpy = jest.spyOn(client, "db").mockImplementationOnce(() => {
+            throw new Error("Database connection error");
+        });
+
+        const res = await request(server).get('/user').query({ sub: "123" });
+
+        expect(res.statusCode).toStrictEqual(500);
+        expect(dbSpy).toHaveBeenCalledWith('get2class');
+        expect(dbSpy).toHaveBeenCalledTimes(1);
+
+        dbSpy.mockRestore();
+    });
+
+    test("Unable to reach users collection", async () => {
+        const userCollectionMock = jest.fn().mockImplementationOnce(() => {
+            throw new Error("Database connection error");
+        });
+
+        const dbSpy = jest.spyOn(client, "db").mockReturnValueOnce({
+            collection: userCollectionMock
+        } as any);
+
+        const res = await request(server).get('/user').query({ sub: "123" });
+
+        expect(res.statusCode).toStrictEqual(500);
+        expect(userCollectionMock).toHaveBeenCalledWith('users');
+        expect(userCollectionMock).toHaveBeenCalledTimes(1);
+        expect(dbSpy).toHaveBeenCalledWith('get2class');
+        expect(dbSpy).toHaveBeenCalledTimes(1);
+    });
+});
