d2c3cbac9388d8b0d6b2b66eddcd33393dc6d98d
trevdang <kfchut12345@gmail.com>
2025-03-18 12:16:47 -0700

    Change sub to be of alphanumeric

--- backend/routes/ScheduleRoutes.ts
+++ backend/routes/ScheduleRoutes.ts
@@ -9,7 +9,7 @@ export const ScheduleRoutes = [
         route: "/schedule",
         action: controller.getSchedule.bind(controller),
         validation: [
-            query("sub").exists().isString(),
+            query("sub").exists().isAlphanumeric(),
             query("term").exists().isString()
         ]
     },
@@ -18,7 +18,7 @@ export const ScheduleRoutes = [
         route: "/schedule",
         action: controller.saveSchedule.bind(controller),
         validation: [
-            body("sub").exists().isString()
+            body("sub").exists().isAlphanumeric()
         ]
     },
     {
@@ -26,7 +26,7 @@ export const ScheduleRoutes = [
         route: "/schedule",
         action: controller.clearSchedule.bind(controller),
         validation: [
-            body("sub").exists().isString()
+            body("sub").exists().isAlphanumeric()
         ]
     },
     {
@@ -34,7 +34,7 @@ export const ScheduleRoutes = [
         route: "/attendance",
         action: controller.getAttendance.bind(controller),
         validation: [
-            query("sub").exists().isString(),
+            query("sub").exists().isAlphanumeric(),
             query("className").exists().isString(),
             query("classFormat").exists().isString(),
             query("term").exists().isString()
@@ -45,7 +45,7 @@ export const ScheduleRoutes = [
         route: "/attendance",
         action: controller.updateAttendance.bind(controller),
         validation: [
-            body("sub").exists().isString(),
+            body("sub").exists().isAlphanumeric(),
             body("className").exists().isString(),
             body("classFormat").exists().isString(),
             body("term").exists().isString()
