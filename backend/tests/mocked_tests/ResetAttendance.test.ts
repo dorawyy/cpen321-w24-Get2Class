a4198ef2c90f959ede54bb8ca1f91eda5af4376d
trevdang <kfchut12345@gmail.com>
2025-03-18 16:21:39 -0700

    Remove unused import

--- backend/tests/mocked_tests/ResetAttendance.test.ts
+++ backend/tests/mocked_tests/ResetAttendance.test.ts
@@ -1,7 +1,6 @@
 import { Server } from 'http';
 import { serverReady, cronResetAttendance, resetAttendanceController } from '../../index';
 import { client } from '../../services';
-import request from 'supertest';
 import { Db } from 'mongodb';
 
 let server: Server;
