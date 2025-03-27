617916c66517a9470183f14cff93f53e60068247
Lucas Beitel <lucasmayimbeitel@gmail.com>
2025-03-26 17:38:57 -0700

    idea for deducting karma

--- backend/controllers/ResetAttendanceController.ts
+++ backend/controllers/ResetAttendanceController.ts
@@ -3,19 +3,31 @@ import { client } from "../services";
 export class ResetAttendanceController {
     async resetAttendance() {
         const allSchedules = await client.db("get2class").collection("schedules").find().toArray();
+
+        let term = getTerm()
+
         for (let schedule of allSchedules) {
             if (schedule.fallCourseList.length != 0) {
                 for (let course of schedule.fallCourseList) {
+                    if (term == "Fall" && course.attended == false) {
+                        // remove some karma
+                    }
                     course.attended = false;
                 }
             }
             if (schedule.winterCourseList.length != 0) {
                 for (let course of schedule.winterCourseList) {
+                    if (term == "Winter" && course.attended == false) {
+                        // remove some karma
+                    }
                     course.attended = false;
                 }
             }
             if (schedule.summerCourseList.length != 0) {
                 for (let course of schedule.summerCourseList) {
+                    if (term == "Summer" && course.attended == false) {
+                        // remove some karma
+                    }
                     course.attended = false
                 }
             }
@@ -36,5 +48,17 @@ export class ResetAttendanceController {
             
             await client.db("get2class").collection("schedules").updateOne(filter, document);
         }
+    }
+}
+
+function getTerm(): string {
+    const month = new Date().getMonth() + 1; 
+
+    if (month >= 1 && month <= 4) {
+        return "Winter";
+    } else if (month >= 5 && month <= 8) {
+        return "Summer";
+    } else {
+        return "Fall";
     }
 }
