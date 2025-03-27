fae522c31772f9b0208506481bfd703f67d44a33
Lucas Beitel <lucasmayimbeitel@gmail.com>
2025-03-27 08:35:38 -0700

    karma deduction ready to test

--- backend/controllers/ResetAttendanceController.ts
+++ backend/controllers/ResetAttendanceController.ts
@@ -4,13 +4,13 @@ export class ResetAttendanceController {
     async resetAttendance() {
         const allSchedules = await client.db("get2class").collection("schedules").find().toArray();
 
-        let term = getTerm()
-
+        const term = getTerm()
         for (let schedule of allSchedules) {
+            let karmaLost = 0
             if (schedule.fallCourseList.length != 0) {
                 for (let course of schedule.fallCourseList) {
                     if (term == "Fall" && course.attended == false) {
-                        // remove some karma
+                        karmaLost += 30
                     }
                     course.attended = false;
                 }
@@ -18,7 +18,7 @@ export class ResetAttendanceController {
             if (schedule.winterCourseList.length != 0) {
                 for (let course of schedule.winterCourseList) {
                     if (term == "Winter" && course.attended == false) {
-                        // remove some karma
+                        karmaLost += 30
                     }
                     course.attended = false;
                 }
@@ -26,11 +26,12 @@ export class ResetAttendanceController {
             if (schedule.summerCourseList.length != 0) {
                 for (let course of schedule.summerCourseList) {
                     if (term == "Summer" && course.attended == false) {
-                        // remove some karma
+                        karmaLost += 30
                     }
                     course.attended = false
                 }
             }
+            deductKarma(schedule.sub, karmaLost)
         }
 
         for (let schedule of allSchedules) {
@@ -60,5 +61,42 @@ function getTerm(): string {
         return "Summer";
     } else {
         return "Fall";
+    }
+}
+
+async function deductKarma(sub: String, karmaLost: number) {
+    let currKarma;
+
+    const userData = await client.db("get2class").collection("users").findOne({ sub });
+    
+    if (userData != null) {
+        currKarma = userData.karma;
+    } else {
+        // handle error
+        // return res.status(400).send("User not found");
+    }
+
+    const filter = {
+        sub
+    };
+
+    const document = {
+        $set: {
+            karma: currKarma - karmaLost
+        },
+    };
+
+    const options = {
+        upsert: false
+    };
+
+    const updateData = await client.db("get2class").collection("users").updateOne(filter, document, options);
+
+    if (!updateData.acknowledged || updateData.modifiedCount == 0) {
+        // handle error
+        // return res.status(400).send("Unable to update karma");
+    } else {
+        // handle success
+        // res.status(200).json({ acknowledged: updateData.acknowledged, message: "Successfully gained karma" });
     }
 }
