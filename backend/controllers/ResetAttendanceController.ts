9439c45f89c5a0821ef62ced587c247346a27e21
trevdang <kfchut12345@gmail.com>
2025-03-29 14:31:24 -0700

    Use .at to index

--- backend/controllers/ResetAttendanceController.ts
+++ backend/controllers/ResetAttendanceController.ts
@@ -54,21 +54,21 @@ export class ResetAttendanceController {
                 if (term == "Fall") {
                     for (let course of schedule.fallCourseList) {
                         const daysBool = JSON.parse(course.daysBool);
-                        if (daysBool[todayIndex as number] == true && course.attended == false) {
+                        if (daysBool.at(todayIndex) == true && course.attended == false) {
                             karmaToDeduct += course.credits * 10;
                         }
                     }
                 } else if (term == "Winter") {
                     for (let course of schedule.winterCourseList) {
                         const daysBool = JSON.parse(course.daysBool);
-                        if (daysBool[todayIndex as number] == true && course.attended == false) {
+                        if (daysBool.at(todayIndex) == true && course.attended == false) {
                             karmaToDeduct += course.credits * 10;
                         }
                     }
                 } else {
                     for (let course of schedule.summerCourseList) {
                         const daysBool = JSON.parse(course.daysBool);
-                        if (daysBool[todayIndex as number] == true && course.attended == false) {
+                        if (daysBool.at(todayIndex) == true && course.attended == false) {
                             karmaToDeduct += course.credits * 10;
                         }
                     }
